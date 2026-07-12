ef0bd7270214e2a62a2ba5b22c8ba88103269233
raphragos-ux <raphragos@gmail.com>
2026-07-12 09:58:17 +0800

    Modify entrypoint.sh for SSL and service paths
    Updated SSL certificate generation and changed paths for key, cert, and config files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Gumawa ng pansamantalang SSL certificate (para sa testing)
-openssl req -x509 -newkey rsa:4096 -keyout /etc/trojan/key.key -out /etc/trojan/cert.crt -days 365 -nodes -subj "/C=PH/ST=Iloilo/L=IloiloCity/O=FreeData/OU=Tech/CN=example.com"
+# Gumawa ng Self-Signed SSL Certificate
+openssl req -x509 -newkey rsa:4096 -keyout /app/key.key -out /app/cert.crt -days 3650 -nodes -subj "/C=PH/ST=Visayas/L=Iloilo/O=FreeData/CN=virgozki.com"
 
-# I-download at i-install ang Trojan binary
-wget -O /usr/bin/trojan https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
-tar -xf /usr/bin/trojan -C /usr/bin/
-chmod +x /usr/bin/trojan
+# Simulan ang Nginx
+nginx
 
-# Simulan ang Nginx sa background
-nginx -g 'daemon off;' &
-
-# Simulan ang Trojan Server
-/usr/bin/trojan -c /etc/trojan/config.json
+# Simulan ang Trojan
+exec trojan -c /app/config.json
